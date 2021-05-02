6e7d470736063a68b11dc830704865c7201814c8
Law Wai Chun <chunlaw@rocketmail.com>
2021-05-02 15:53:04 +0800

    Add basic testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { AppContextProvider } from './AppContext'
+import './i18n'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App', () => {
+  render(
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  );
+  const initializingElement = screen.getByText(/初始設定/);
+  expect(initializingElement).toBeInTheDocument();
 });
