6888761df8699071c9fd3aea84830e8ad56913d1
hk-ng <4487940+hk-ng@users.noreply.github.com>
2021-09-07 09:44:04 +0800

    Allow automatic update using service worker (#16)
    * add workbox-window. auto refresh on update
    
    * add github job id into service worker to trigger update
    
    * Update deploy.yml

--- src/index.js
+++ src/index.js
@@ -65,7 +65,11 @@ if (isHuman()){
   // If you want your app to work offline and load faster, you can change
   // unregister() to register() below. Note this comes with some pitfalls.
   // Learn more about service workers: https://cra.link/PWA
-  serviceWorkerRegistration.register();
+  serviceWorkerRegistration.register({
+    onUpdate: (registration, skipWaiting, installingServiceWorker) => {
+      skipWaiting();
+    },
+  });
 
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
