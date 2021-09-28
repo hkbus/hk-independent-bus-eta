e8e19eb524bfecf52aeddf9c146b0b1a55932906
Law Wai Chun <chunlaw@rocketmail.com>
2021-09-28 11:15:10 +0800

    Eliminate makeStyles

--- src/components/route-list/RouteNo.tsx
+++ src/components/route-list/RouteNo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Typography } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
+import { styled } from "@mui/material/styles";
 
 interface RouteNoProps {
   routeNo: string;
@@ -9,39 +9,45 @@ interface RouteNoProps {
 }
 
 const RouteNo = ({ routeNo, component, align }: RouteNoProps) => {
-  useStyles();
   const [prefix, suffix] = routeNo.match(/[A-Z]+$/)
     ? [routeNo.slice(0, -1), routeNo.slice(-1)]
     : [routeNo, ""];
   return (
-    <Typography
+    <RouteNoTypography
+      // @ts-ignore
       component={component || "h2"}
       align={align}
       variant="caption"
       color="textPrimary"
-      className={"routeNo-root"}
+      className={classes.root}
     >
-      <span className={"routeNo-routePrefix"}>{prefix}</span>
-      <span className={"routeNo-routeSuffix"}>{suffix}</span>
-    </Typography>
+      <span className={classes.prefix}>{prefix}</span>
+      <span className={classes.suffix}>{suffix}</span>
+    </RouteNoTypography>
   );
 };
 
 export default RouteNo;
 
-const useStyles = makeStyles((theme) => ({
-  "@global": {
-    ".routeNo-root": {
-      lineHeight: "normal",
-      display: "inline",
-    },
-    ".routeNo-routePrefix": {
-      fontSize: "1.2rem",
-      fontFamily: '"Oswald", sans-serif',
-    },
-    ".routeNo-routeSuffix": {
-      fontSize: "0.95rem",
-      fontFamily: '"Oswald", sans-serif',
-    },
+const PREFIX = 'routeNo';
+
+const classes = {
+  root: `${PREFIX}-root`,
+  prefix: `${PREFIX}-prefix`,
+  suffix: `${PREFIX}-suffix`
+}
+
+const RouteNoTypography = styled(Typography)(({theme}) => ({
+  [`&.${classes.root}`]: {
+    lineHeight: "normal",
+    display: "inline",
+  },
+  [`& .${classes.prefix}`]: {
+    fontSize: "1.2rem",
+    fontFamily: '"Oswald", sans-serif',
+  },
+  [`& .${classes.suffix}`]: {
+    fontSize: "0.95rem",
+    fontFamily: '"Oswald", sans-serif',
   },
 }));
