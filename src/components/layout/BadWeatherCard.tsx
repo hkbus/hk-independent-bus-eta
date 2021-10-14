349be00ee295b02a6fb6095636dd39aefe202361
thomassth <thomassth@users.noreply.github.com>
2021-10-14 20:43:22 +0000

    Prettified Code!

--- src/components/layout/BadWeatherCard.tsx
+++ src/components/layout/BadWeatherCard.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { Card, CardContent, styled, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 // eslint-disable-next-line
-const adverseCode = new RegExp('((\"code\":\")+(TC8+|TC9|TC10|WRAINR|WRAINB))');
+const adverseCode = new RegExp('(("code":")+(TC8+|TC9|TC10|WRAINR|WRAINB))');
 
-const useFetch = url => {
+const useFetch = (url) => {
   const [data, setData] = useState(null);
   async function fetchData() {
     const response = await fetch(url);
@@ -13,10 +13,11 @@ const useFetch = url => {
     //Test string, comment in production
     // setData({ "WTCSGNL": { "name": "Tropical Cyclone Warning Signal", "code": "TC8NE", "actionCode": "ISSUE", "type": "No. 8 Northeast Gale or Storm Signal", "issueTime": "2021-10-12T17:20:00+08:00", "updateTime": "2021-10-12T17:20:00+08:00" } });
   }
-  useEffect(() => { fetchData() }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
   return data;
 };
-
 
 function BadWeatherCard() {
   let match = false;
@@ -24,29 +25,30 @@ function BadWeatherCard() {
   //Hook cannot go under if()
   const { t } = useTranslation();
   let bwLink = t("bad-weather-link");
-  let bwText = t("bad-weather-text")
-  const data = useFetch("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum&lang=en");
+  let bwText = t("bad-weather-text");
+  const data = useFetch(
+    "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum&lang=en"
+  );
 
   string = JSON.stringify(data);
   match = adverseCode.test(string);
   if (match) {
-    return clickCard(bwText, bwLink)
+    return clickCard(bwText, bwLink);
   } else {
-    return null
+    return null;
   }
 }
-
 
 const StyledCard = styled(Card)(({ theme }) => ({
   backgroundColor: theme.palette.main,
   borderRadius: theme.shape.borderRadius,
-  margin: 0.2
+  margin: 0.2,
 }));
 
-const StyledLink = styled('a')(({ theme }) => ({
-  textDecorationLine: 'none',
+const StyledLink = styled("a")(({ theme }) => ({
+  textDecorationLine: "none",
   color: theme.palette.text.primary,
-}))
+}));
 
 const clickCard = (content, link) => {
   return (
@@ -58,6 +60,6 @@ const clickCard = (content, link) => {
       </StyledLink>
     </StyledCard>
   );
-}
+};
 
 export default BadWeatherCard;
