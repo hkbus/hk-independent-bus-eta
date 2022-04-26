0d862facd9fc98e7d80c8bb65995e1ec1ad544c3
sdip15fa <91375035+sdip15fa@users.noreply.github.com>
2022-04-26 16:52:33 +0800

    (minor changes) add overflow to privacy & terms, and use yarn 1? (#37)
    * default dark mode
    
    * Create quickdeploy.yml
    
    * Update quickdeploy.yml
    
    * Update quickdeploy.yml
    
    * Update quickdeploy.yml
    
    * Update quickdeploy.yml
    
    * add overflow to privacy & terms
    
    * delete quickdeploy.yml
    
    * it is 2022, upgrade to node 16.x & 17.x
    
    Co-authored-by: wcyat <9657150-wcyat@users.noreply.gitlab.com>
    Co-authored-by: wcyat <sleep@wcyat.me>

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -3,7 +3,7 @@ import { Paper, Link } from "@mui/material";
 
 const PrivacyPolicy = () => {
   return (
-    <Paper sx={{ p: 2 }}>
+    <Paper sx={{ p: 2, maxHeight: "100%", overflow: "auto" }}>
       <strong>Privacy Policy</strong>{" "}
       <p>
         HKBUS.APP built the hkbus.app app as an Open Source app. This SERVICE is
--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -3,7 +3,7 @@ import { Paper, Link } from "@mui/material";
 
 const TermsAndConditions = () => {
   return (
-    <Paper sx={{ p: 2 }}>
+    <Paper sx={{ p: 2, maxHeight: "100%", overflow: "auto" }}>
       <strong>Terms &amp; Conditions</strong>{" "}
       <p>
         By downloading or using the app, these terms will automatically apply to
