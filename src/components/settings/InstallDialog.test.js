9d2587298b2811bac11f90c0f4422b316ab10a56
wcyat <wcyat@wcyat.me>
2022-06-18 00:42:16 +0800

    fix test

--- src/components/settings/InstallDialog.test.js
+++ src/components/settings/InstallDialog.test.js
@@ -1,17 +1,15 @@
-import { act, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "../../i18n";
 import InstallDialog from "./InstallDialog";
 
 test("renders App", async () => {
-  act(() => {
-    const theme = createTheme();
-    render(
-      <ThemeProvider theme={theme}>
-        <InstallDialog open />
-      </ThemeProvider>
-    );
-  });
+  const theme = createTheme();
+  render(
+    <ThemeProvider theme={theme}>
+      <InstallDialog open />
+    </ThemeProvider>
+  );
   const initializingElement = screen.getByText(/iOS/);
   expect(initializingElement).toBeInTheDocument();
 });
