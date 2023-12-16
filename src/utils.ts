4e327b337c7e398f61c6275541c2d291575c6afc
maruk0chan <marcochan94@hotmail.com>
2023-12-16 13:57:23 +0900

    export default location lat and lng

--- src/utils.ts
+++ src/utils.ts
@@ -31,7 +31,7 @@ export const getDistanceWithUnit = (a: GeoLocation, b: GeoLocation) => {
   };
 };
 
-const defaultLocation = { lat: 22.302711, lng: 114.177216 };
+export const defaultLocation = { lat: 22.302711, lng: 114.177216 };
 // HK location if no valid value
 export const checkPosition = (position?: GeoLocation): GeoLocation => {
   if (
