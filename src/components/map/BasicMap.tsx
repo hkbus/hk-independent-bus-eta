775d25b6e398a729b061f05731b3474130eb47e9
maruk0chan <marcochan94@hotmail.com>
2023-12-19 10:48:25 +0900

    adjust map container size

--- src/components/map/BasicMap.tsx
+++ src/components/map/BasicMap.tsx
@@ -2,7 +2,12 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export const BasicMap = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      style={{ height: "100%" }}
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
