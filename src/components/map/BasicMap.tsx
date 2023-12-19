32d9efdb8937d612503cabcb74341e80425565ea
maruk0chan <marcochan94@hotmail.com>
2023-12-19 10:38:41 +0900

    add BasicMap component

---
+++ src/components/map/BasicMap.tsx
@@ -0,0 +1,17 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export const BasicMap = () => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
