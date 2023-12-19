196bfd05d833baebdb8e22417f1f6b67390f928e
maruk0chan <marcochan94@hotmail.com>
2023-12-20 01:11:55 +0900

    create a draft map

--- src/components/map/BasicMap.tsx
+++ src/components/map/BasicMap.tsx
@@ -2,48 +2,50 @@ import { LatLngExpression } from "leaflet";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import {
-  MapContainer,
-  TileLayer
-} from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { defaultLocation } from "../../utils";
 
-const center = [defaultLocation.lat, defaultLocation.lng] as LatLngExpression;
+const defaultCenter = [defaultLocation.lat, defaultLocation.lng] as LatLngExpression;
 
-const zoom = 13
+const zoom = 13;
 
 function DisplayPosition({ map }) {
-  const [position, setPosition] = useState(() => map.getCenter())
+  const [position, setPosition] = useState(() => map.getCenter());
 
   const onClick = useCallback(() => {
-    map.setView(center, zoom)
-  }, [map])
+    map.setView(defaultCenter, zoom);
+  }, [map]);
 
   const onMove = useCallback(() => {
-    setPosition(map.getCenter())
-  }, [map])
+    setPosition(map.getCenter());
+  }, [map]);
 
   useEffect(() => {
-    map.on('move', onMove)
+    map.on("move", onMove);
     return () => {
-      map.off('move', onMove)
-    }
-  }, [map, onMove])
+      map.off("move", onMove);
+    };
+  }, [map, onMove]);
 
   return (
     <p>
-      latitude: {position.lat.toFixed(4)}, longitude: {position.lng.toFixed(4)}{' '}
+      latitude: {position.lat.toFixed(4)}, longitude: {position.lng.toFixed(4)}{" "}
       <button onClick={onClick}>reset</button>
     </p>
-  )
+  );
 }
 
 export const BasicMap = () => {
   const [map, setMap] = useState(null);
+  const [center, setCenter] = useState<LatLngExpression>([
+    defaultLocation.lat,
+    defaultLocation.lng,
+  ]);
 
   const displayMap = useMemo(
     () => (
       <MapContainer
+        style={{ height: "100%" }}
         center={center}
         zoom={zoom}
         scrollWheelZoom={false}
@@ -53,15 +55,20 @@ export const BasicMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Marker position={center}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
     ),
-    []
+    [center]
   );
 
   return (
-    <div>
+    <>
       {map ? <DisplayPosition map={map} /> : null}
       {displayMap}
-    </div>
+    </>
   );
 };
