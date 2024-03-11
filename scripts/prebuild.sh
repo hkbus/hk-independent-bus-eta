13c5c4eb4893e9e24ff0b657c8e645a1b3b4763b
Law Wai Chun <chunlaw@rocketmail.com>
2024-03-11 23:32:24 +0800

    Migrate to sw.js from service-worker.js

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,15 +1,15 @@
-if [ -z "$VITE__COMMIT_HASH" ];
-then export VITE__COMMIT_HASH=$(git rev-parse --short HEAD);
+if [ -z "$VITE_COMMIT_HASH" ];
+then export VITE_COMMIT_HASH=$(git rev-parse --short HEAD);
 fi;
 
-if [ -z "$VITE__COMMIT_MESSAGE" ];
-then export VITE__COMMIT_MESSAGE="$(git log -1 --pretty=format:%s  $*)";
+if [ -z "$VITE_COMMIT_MESSAGE" ];
+then export VITE_COMMIT_MESSAGE="$(git log -1 --pretty=format:%s  $*)";
 fi;
 
-if [ -z "$VITE__VERSION" ];
-then export VITE__VERSION="$(node -e 'console.log(require("./package.json").version)')";
+if [ -z "$VITE_VERSION" ];
+then export VITE_VERSION="$(node -e 'console.log(require("./package.json").version)')";
 fi;
 
-if [ -z "$VITE__REPO_URL" ];
-then export VITE__REPO_URL="$(git config --get remote.origin.url)";
+if [ -z "$VITE_REPO_URL" ];
+then export VITE_REPO_URL="$(git config --get remote.origin.url)";
 fi;
---
+++ src/service-worker.js
@@ -0,0 +1,1 @@
+importScripts('sw.js');
