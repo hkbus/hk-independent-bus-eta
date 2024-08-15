d3ca25693977fb404d8d394ede7f9af266364a12
Law Wai Chun <chunlaw@rocketmail.com>
2024-08-16 00:14:39 +0800

    Resume routine deployment

--- src/components/layout/NoticeCard.tsx
+++ src/components/layout/NoticeCard.tsx
@@ -1,18 +1,49 @@
 import { Box, Paper, SxProps, Theme, Typography } from "@mui/material";
-import ErrorIcon from "@mui/icons-material/Error";
+import WarnIcon from "@mui/icons-material/Warning";
+import useLanguage from "../../hooks/useTranslation";
+import { useCallback, useEffect, useState } from "react";
+import { iOSRNWebView } from "../../utils";
+import { Language } from "../../data";
+
+interface NoticeCardState {
+  content: Record<Language, string[]>;
+  url: string;
+  enableLinkInIos: boolean;
+  isShown: boolean;
+}
 
 const NoticeCard = () => {
+  const language = useLanguage();
+  const [state, setState] = useState<NoticeCardState | null>(null);
+
+  useEffect(() => {
+    console.log("hihi");
+    fetch("/notice.json")
+      .then((r) => r.json())
+      .then((r) => setState(r));
+  }, []);
+
+  const handleClick = useCallback(() => {
+    if (state === null) return;
+    if (iOSRNWebView() && !state.enableLinkInIos) {
+      return;
+    }
+    window.open(state.url, "_target");
+  }, [state]);
+
+  if (state === null || !state.isShown) {
+    return null;
+  }
+
   return (
-    <Paper
-      variant="outlined"
-      sx={rootSx}
-      onClick={() => window.open("https://t.me/hkbusapp", "_target")}
-    >
-      <ErrorIcon color="error" />
+    <Paper variant="outlined" sx={rootSx} onClick={handleClick}>
+      <WarnIcon color="warning" />
       <Box>
-        <Typography>因資料一線通提供的 API 出現異常</Typography>
-        <Typography>hkbus.app 暫未能提供九巴到站預報</Typography>
-        <Typography>我們已向 1823 求助</Typography>
+        {state.content[language].map((v, idx) => (
+          <Typography key={`_notice-${idx}`} variant="subtitle2">
+            {v}
+          </Typography>
+        ))}
       </Box>
     </Paper>
   );
@@ -27,6 +58,6 @@ const rootSx: SxProps<Theme> = {
   py: 1,
   alignItems: "center",
   textAlign: "left",
-  gap: 1,
+  gap: 2,
   display: "flex",
 };
