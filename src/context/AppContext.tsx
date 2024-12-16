17c4b097951694539a927ca7a5522d5b3f3669dd
desmondlsl <68117020+desmondlsl@users.noreply.github.com>
2024-12-16 16:04:33 +0800

    Follow system color scheme (#210)
    * follow system color scheme
    
    * fix a short flash at outside app during start
    
    * Update AppContext.tsx

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -593,19 +593,62 @@ export const AppContextProvider = ({ children }: AppContextProviderProps) => {
     );
   }, []);
 
-  const [colorMode, setColorMode] = useState<"light" | "dark">("light");
-  useEffect(() => {
+  const calculateColorMode = useCallback(() => {
     if (state._colorMode === "light" || state._colorMode === "dark") {
-      setColorMode(state._colorMode);
+      return state._colorMode;
     } else {
-      const mql = window.matchMedia("(prefers-color-scheme: light)");
-      setColorMode(mql.matches ? "light" : "dark");
-      const themeListener = (e: MediaQueryListEvent) =>
-        setColorMode(e.matches ? "light" : "dark");
-      mql?.addEventListener("change", themeListener);
-      return () => mql?.removeEventListener("change", themeListener);
+      if (
+        window.systemColorScheme?.value &&
+        ["light", "dark"].includes(window.systemColorScheme?.value)
+      ) {
+        return window.systemColorScheme?.value;
+      }
+      return window.matchMedia("(prefers-color-scheme: light)").matches
+        ? "light"
+        : "dark";
     }
-  }, [state._colorMode, setColorMode]);
+  }, [state._colorMode]);
+  const [colorMode, setColorMode] = useState<"light" | "dark">(
+    calculateColorMode()
+  );
+  useEffect(() => {
+    setColorMode(calculateColorMode());
+
+    if (state._colorMode === "system") {
+      const themeListener = () => setColorMode(calculateColorMode());
+
+      const mql = window.matchMedia("(prefers-color-scheme: light)");
+      mql.addEventListener("change", themeListener);
+      if (
+        window.systemColorSchemeCallbacks &&
+        Array.isArray(window.systemColorSchemeCallbacks)
+      ) {
+        window.systemColorSchemeCallbacks.push(themeListener);
+      }
+
+      return () => {
+        mql.removeEventListener("change", themeListener);
+        if (
+          window.systemColorSchemeCallbacks &&
+          Array.isArray(window.systemColorSchemeCallbacks)
+        ) {
+          window.systemColorSchemeCallbacks =
+            window.systemColorSchemeCallbacks.filter(
+              (cb) => cb !== themeListener
+            );
+        }
+      };
+    }
+  }, [state._colorMode, calculateColorMode]);
+
+  useEffect(() => {
+    window.ReactNativeWebView?.postMessage(
+      JSON.stringify({
+        type: "color-mode",
+        value: colorMode,
+      })
+    );
+  }, [colorMode]);
 
   const importAppState = useCallback((appState: AppState) => {
     setStateRaw(appState);
