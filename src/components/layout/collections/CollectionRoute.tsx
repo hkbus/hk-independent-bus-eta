c5c73bb1e78f5b27e25ce52890e9ca7364de077c
Law Wai Chun <chunlaw@rocketmail.com>
2026-02-18 12:18:28 +0800

    Enable user to delete ETA

--- src/components/layout/collections/CollectionRoute.tsx
+++ src/components/layout/collections/CollectionRoute.tsx
@@ -1,12 +1,22 @@
 import { useState, useContext, useCallback } from "react";
 import { DragDropContext, Draggable, DropResult } from "react-beautiful-dnd";
 import { useTranslation } from "react-i18next";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
+import {
+  EditOutlined as EditIcon,
+  Code as ReorderIcon,
+} from "@mui/icons-material";
 import SuccinctTimeReport from "../../home/SuccinctTimeReport";
 import { reorder } from "../../../utils";
 import Droppable from "../../StrictModeDroppable";
 import DbContext from "../../../context/DbContext";
 import CollectionContext from "../../../CollectionContext";
+import { ManageMode } from "../../../data";
 
 const CollectionRoute = () => {
   const { t } = useTranslation();
@@ -20,6 +30,7 @@ const CollectionRoute = () => {
     savedEtas,
     setSavedEtas,
   } = useContext(CollectionContext);
+  const [mode, setMode] = useState<ManageMode>("order");
 
   const [items, setItems] = useState<string[]>(() => {
     if (collectionIdx === null) return [];
@@ -46,8 +57,36 @@ const CollectionRoute = () => {
     [items, collectionIdx, setSavedEtas, setCollectionEtas]
   );
 
+  const handleDelete = useCallback(
+    (eta: string) => {
+      const newItems = items.filter((v) => v !== eta);
+      if (collectionIdx === -1) {
+        setSavedEtas(newItems);
+      } else {
+        setCollectionEtas(newItems);
+      }
+      setItems(newItems);
+    },
+    [items, collectionIdx, setSavedEtas, setCollectionEtas]
+  );
+
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
+      <Box display="flex" justifyContent="flex-end">
+        <ToggleButtonGroup
+          size="small"
+          value={mode}
+          onChange={(_, v) => setMode(v)}
+          exclusive
+        >
+          <ToggleButton value="order">
+            <ReorderIcon sx={{ transform: "rotate(90deg)" }} fontSize="small" />
+          </ToggleButton>
+          <ToggleButton value="edit">
+            <EditIcon fontSize="small" />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
       <Droppable droppableId="saved-eta-list">
         {(provided) => (
           <Box ref={provided.innerRef} {...provided.droppableProps}>
@@ -57,6 +96,8 @@ const CollectionRoute = () => {
                   item={eta}
                   index={index}
                   key={`savedEta-${eta}`}
+                  mode={mode}
+                  onDelete={() => handleDelete(eta)}
                 />
               ))
             ) : (
@@ -77,17 +118,24 @@ export default CollectionRoute;
 interface DraggableListItemProps {
   item: string;
   index: number;
+  mode: ManageMode;
+  onDelete: () => void;
 }
 
-const DraggableListItem = ({ item, index }: DraggableListItemProps) => (
-  <Draggable draggableId={item} index={index}>
+const DraggableListItem = ({
+  item,
+  index,
+  mode,
+  onDelete,
+}: DraggableListItemProps) => (
+  <Draggable draggableId={item} index={index} isDragDisabled={mode !== "order"}>
     {(provided) => (
       <Box
         ref={provided.innerRef}
         {...provided.draggableProps}
         {...provided.dragHandleProps}
       >
-        <SuccinctTimeReport routeId={item} mode="order" />
+        <SuccinctTimeReport routeId={item} mode={mode} onDelete={onDelete} />
       </Box>
     )}
   </Draggable>
