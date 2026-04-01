0f5f5944119f5559065b774fe89f63af6275b1c9
Law Wai Chun <chunlaw@rocketmail.com>
2026-03-31 19:46:34 -0700

    Enable use of notice array

--- src/components/layout/NoticeCard.tsx
+++ src/components/layout/NoticeCard.tsx
@@ -3,6 +3,8 @@ import {
   IconButton,
   Paper,
   SxProps,
+  Tab,
+  Tabs,
   Theme,
   Typography,
 } from "@mui/material";
@@ -13,8 +15,10 @@ import { iOSRNWebView } from "../../utils";
 import { Language } from "../../data";
 import { Close as CloseIcon } from "@mui/icons-material";
 import AppContext from "../../context/AppContext";
+import SwipeableViews from "react-swipeable-views";
 
 interface NoticeCardState {
+  id: string;
   content: Record<Language, string[]>;
   url: string;
   link: Record<Language, string>;
@@ -25,62 +29,99 @@ interface NoticeCardState {
 
 const NoticeCard = () => {
   const language = useLanguage();
-  const [state, setState] = useState<NoticeCardState | null>(null);
-  const [closeNoticeContent, setCloseNoticeContent] = useState<string>(
-    localStorage.getItem("closeNoticeContent") ?? ""
+  const [state, setState] = useState<NoticeCardState[]>([]);
+  const [viewIdx, setViewIdx] = useState<number>(0);
+  const [closeNoticeIds, setCloseNoticeIds] = useState<string[]>(
+    JSON.parse(localStorage.getItem("closeNoticeIds") ?? "[]")
   );
   const { openUrl } = useContext(AppContext);
 
+  const handleClick = useCallback(
+    (idx: number) => () => {
+      if (idx < state.length) return;
+      if (iOSRNWebView() && !state[idx].enableLinkInIos) {
+        return;
+      }
+      openUrl(state[idx].link[language]);
+    },
+    [language, openUrl, state]
+  );
+
+  const closeNotice = useCallback(
+    (id: string) => () => {
+      setCloseNoticeIds((prev) => [...prev, id]);
+    },
+    []
+  );
+
   useEffect(() => {
-    fetch("https://notice.hkbus.app/notice.json")
+    fetch("https://notice.hkbus.app/notices.json")
       .then((r) => r.json())
-      .then((r) =>
-        setState({
-          ...r,
-          endDate: new Date(r.endDate),
-        })
-      );
-  }, []);
-
-  const handleClick = useCallback(() => {
-    if (state === null) return;
-    if (iOSRNWebView() && !state.enableLinkInIos) {
-      return;
-    }
-    openUrl(state.link[language]);
-  }, [language, openUrl, state]);
-
-  const closeNotice = useCallback(() => {
-    if (state) {
-      localStorage.setItem("closeNoticeContent", JSON.stringify(state.content));
-      setCloseNoticeContent(JSON.stringify(state.content));
-    }
-  }, [state]);
+      .then((r: NoticeCardState[]) =>
+        setState(
+          r
+            .filter(({ id }) => !closeNoticeIds.includes(id))
+            .map((notice) => ({
+              ...notice,
+              endDate: new Date(notice.endDate),
+            }))
+            .filter((notice) => notice.endDate >= new Date())
+        )
+      )
+      .finally(() => {
+        localStorage.setItem("closeNoticeIds", JSON.stringify(closeNoticeIds));
+      });
+  }, [closeNoticeIds]);
 
   if (
-    state === null ||
-    closeNoticeContent === JSON.stringify(state.content) ||
-    !state.isShown ||
-    state.endDate < new Date()
+    state.length === 0 ||
+    !state.reduce((acc, cur) => acc || cur.isShown, false)
   ) {
     return null;
   }
 
   return (
     <Paper variant="outlined" sx={rootSx}>
-      <WarnIcon color="warning" />
-      <Box onClick={handleClick} sx={{ cursor: "pointer" }}>
-        {state.content[language].map((v, idx) => (
-          <Typography
-            key={`_notice-${idx}`}
-            variant="subtitle2"
-            sx={{ height: "3.14em", overflowY: "auto" }}
-          >
-            {v}
-          </Typography>
-        ))}
+      <Box
+        display="flex"
+        justifyContent="flex-start"
+        flexDirection="column"
+        gap={0.5}
+      >
+        <Tabs
+          sx={noticeTabsSx}
+          value={viewIdx}
+          onChange={(_, v) => setViewIdx(v)}
+        >
+          {state.map((_, idx) => (
+            <Tab key={`notice-tab-${idx}`} label={""} value={idx} />
+          ))}
+        </Tabs>
+        <SwipeableViews
+          index={viewIdx}
+          onChangeIndex={(idx) => {
+            setViewIdx(idx);
+          }}
+        >
+          {state.map((notice) => (
+            <Box display="flex" alignItems="center" gap={2} key={notice.id}>
+              <WarnIcon color="warning" />
+              <Box onClick={handleClick(viewIdx)} sx={{ cursor: "pointer" }}>
+                {notice.content[language].map((v, idx) => (
+                  <Typography
+                    key={`_notice-${idx}`}
+                    variant="subtitle2"
+                    sx={{ height: "3.14em", overflowY: "auto" }}
+                  >
+                    {v}
+                  </Typography>
+                ))}
+              </Box>
+            </Box>
+          ))}
+        </SwipeableViews>
       </Box>
-      <IconButton size="small" onClick={closeNotice}>
+      <IconButton size="small" onClick={closeNotice(state[viewIdx].id)}>
         <CloseIcon />
       </IconButton>
     </Paper>
@@ -97,4 +138,16 @@ const rootSx: SxProps<Theme> = {
   textAlign: "left",
   gap: 2,
   display: "flex",
+};
+
+const noticeTabsSx: SxProps<Theme> = {
+  minHeight: 5,
+  ["& .MuiTab-root"]: {
+    px: 0,
+    py: 0,
+    height: 5,
+    borderBottom: "#7777 1px solid",
+    minHeight: 5,
+    mx: 1,
+  },
 };
