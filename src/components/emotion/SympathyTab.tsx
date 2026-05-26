2d153b0e6e563c9ed556aaca935e4b4238db06b2
Law Wai Chun <chunlaw@rocketmail.com>
2026-05-25 17:22:19 -0700

    update sympathy page

--- src/components/emotion/SympathyTab.tsx
+++ src/components/emotion/SympathyTab.tsx
@@ -10,7 +10,7 @@ const SympathyTab = () => {
   const endDate = new Date(); // Or specify a target end date
 
   const differenceInMs = endDate.getTime() - startDate.getTime();
-  const imgIdx = `${(Math.floor(differenceInMs / (1000 * 60 * 60 * 24)) % SYMPATHY_IMAGE_COUNT + 1)}`.padStart(3, "0");
+  const imgIdx = `${(Math.floor(differenceInMs / (1000 * 60 * 60 * 24)) % SYMPATHY_IMAGE_COUNT + 2)}`.padStart(3, "0");
 
   return (
     <Box sx={rootSx}>
@@ -18,26 +18,36 @@ const SympathyTab = () => {
         src={`/img/sympathy/${imgIdx}.png`}
         style={{width: "100%"}}
       />
+      <img
+          src={`/img/sympathy/logo.png`}
+          style={{
+            width: 56,
+            height: 56,
+            marginTop: -80,
+          }}
+          onClick={() => openUrl('https://www.ediversity.org/')}
+        />
       <Box sx={{
         display: "flex",
         alignItems: 'center',
         justifyContent: 'center',
         width: '100%',
+        gap: 1,
       }}>
         <img
           src={`/img/sympathy/text.png`}
           style={{
-            width: 185,
-            height: 36,
+            width: 231,
+            height: 28,
           }}
         />
         <img
           src={`/img/sympathy/button.png`}
           style={{
-            width: 36,
-            height: 36,
+            width: 28,
+            height: 28,
           }}
-          onClick={() => openUrl('https://www.ediversity.org/')}
+          onClick={() => openUrl('https://wa.me/85253634035')}
         />
       </Box>
     </Box>
@@ -49,7 +59,7 @@ export default SympathyTab;
 const rootSx: SxProps<Theme> = {
   display: "flex",
   flexDirection: "column",
-  alignItems: "start",
+  alignItems: "center",
   flex: 1,
   gap: 4,
   p: 2,
