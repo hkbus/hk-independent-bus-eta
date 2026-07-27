45b16d618619cf56080afde4a56ad0b1b2e17e9f
Evan Chan <37951241+evnchn@users.noreply.github.com>
2026-07-27 10:03:27 +0800

    a11y: add alt text to HKO weather-warning icons (#294)

--- src/i18n/translation.js
+++ src/i18n/translation.js
@@ -28,6 +28,7 @@ const resources = {
       對調起點及終點: "Swap origin and destination",
       跳至主要內容: "Skip to main content",
       輸入巴士路線號碼搜尋: "Search by entering a bus route number",
+      天氣警告: "Weather warning",
       離線: "Offline",
       首頁: "Home",
       搜尋: "Search",
